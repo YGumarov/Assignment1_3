83159f95ec7406ed8a3c9448598c729d17d9f02f
unknown <yerassylgumarov@gmail.com>
2023-04-09 12:37:24 +0600

    Initial commit

---
+++ Assignment1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿static bool isPrime(int n,int temp=2)
+{
+    if (temp == n){ return true;}
+    if (n % temp == 0) {return false;} 
+    bool result=isPrime(n,temp+1);
+    return result;
+}
+
+int n = int.Parse(Console.ReadLine());
+if (isPrime(n) == true)
+{
+    Console.WriteLine("Prime");
+}
+else
+{
+    Console.WriteLine("Composite");
+}
